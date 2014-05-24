italiangrid/voms

commit 8ed971b34b8288baf933079b9a4e29060799c350

Make sure OpenSSL also uses the voms verification time. VOMS has an API call to set the verification time, VOMS_SetVerificationTime(), to set the time at which an AC should be valid. However, the certificate(chain) used to sign the AC (i.e. the VOMS server certificate and chain) is checked using OpenSSLs X509_verify_cert() which checks it at the current time. The logical time to check that chain would be the same time as used for the AC itself. In order to do that, we call the OpenSSL X509_VERIFY_PARAM_set_time().

src/api/ccapi/api_util.cc
@@ -736,9 +736,16 @@ vomsdata::check_cert(STACK_OF(X509) *stack)
 
         ERR_clear_error();
         error = VERR_VERIFY;
-        X509_STORE_CTX_init(csc, ctx, sk_X509_value(stack, 0), NULL);
-        X509_STORE_CTX_set_ex_data(csc, PVD_STORE_EX_DATA_IDX, pvd);
-        index = X509_verify_cert(csc);
+        if (X509_STORE_CTX_init(csc, ctx, sk_X509_value(stack, 0), NULL)==0) {
+	    error = VERR_MEM;
+	} else	{
+	    X509_STORE_CTX_set_ex_data(csc, PVD_STORE_EX_DATA_IDX, pvd);
+	    /* X509_STORE_CTX_get0_param() only returns NULL if
+	     * X509_STORE_CTX_init() has failed  */
+	    X509_VERIFY_PARAM_set_time(X509_STORE_CTX_get0_param(csc),
+				       verificationtime);
+	    index = X509_verify_cert(csc);
+	}
       }
     }
 #ifdef SIGPIPE

commit 27c609d8924709c9de0c240e268535e79dfc3fbe

Fixes verificationtime issue.
Commit 8ed971b34b8288baf933079b9a4e29060799c350 introduced an issue
that lead to failures in recognizing valid VOMS attributes in client
requests.

Now verificationtime is set for OpenSSL verification only if it
was actually set by the caller.

This commit improves error reporting in APIs.

src/api/ccapi/api_util.cc
@@ -667,21 +667,26 @@ X509 *vomsdata::check_from_file(AC *ac, std::ifstream &file, const std::string &
 
   X509 *cert = X509_dup(sk_X509_value(certstack, 0));
 
-  bool found = false;
+  if ( ! check_sig_ac(cert,ac) ){
 
-  if (check_sig_ac(cert, ac))
-    found = true;
-  else
-    seterror(VERR_SIGN, "Unable to verify signature!");
+    char * cert_subject = X509_NAME_oneline(X509_get_subject_name(cert), NULL, 0);
+    std::string msg("AC signature check failed for certificate '");
 
-  if (found) {
-    if (!check_cert(certstack)) {
-      cert = NULL;
-      seterror(VERR_SIGN, "Unable to verify certificate chain.");
+    msg += cert_subject;
+    msg += std::string("' for vo ");
+    msg += voname;
+
+    seterror(VERR_SIGN, msg);
+    OPENSSL_free(cert_subject);
+
+  } else {
+
+    if ( ! check_cert(certstack) ) {
+      // check_cert sets the error message
+      cert = 0;
     }
+
   }
-  else
-    seterror(VERR_SIGN, std::string("Cannot find certificate of AC issuer for vo ") + voname);
 
   AC_CERTS_free(certs);
   return cert;
@@ -716,14 +721,16 @@ vomsdata::check_cert(STACK_OF(X509) *stack)
   csc = X509_STORE_CTX_new();
   ctx = X509_STORE_new();
   error = VERR_MEM;
+
   if (ctx && csc) {
     proxy_verify_desc *pvd = setup_initializers(strdup((char*)ca_cert_dir.c_str()));
 
     X509_STORE_set_verify_cb_func(ctx,proxy_verify_callback);
-    
+
 #ifdef SIGPIPE
     void (*oldsignal)(int) = signal(SIGPIPE,SIG_IGN);
 #endif
+
     CRYPTO_malloc_init();
     if ((lookup = X509_STORE_add_lookup(ctx, X509_LOOKUP_file()))) {
       X509_LOOKUP_load_file(lookup, NULL, X509_FILETYPE_DEFAULT);
@@ -737,22 +744,50 @@ vomsdata::check_cert(STACK_OF(X509) *stack)
         ERR_clear_error();
         error = VERR_VERIFY;
         if (X509_STORE_CTX_init(csc, ctx, sk_X509_value(stack, 0), NULL)==0) {
-	    error = VERR_MEM;
-	} else	{
-	    X509_STORE_CTX_set_ex_data(csc, PVD_STORE_EX_DATA_IDX, pvd);
-	    /* X509_STORE_CTX_get0_param() only returns NULL if
-	     * X509_STORE_CTX_init() has failed  */
-	    X509_VERIFY_PARAM_set_time(X509_STORE_CTX_get0_param(csc),
-				       verificationtime);
-	    index = X509_verify_cert(csc);
-	}
+          error = VERR_MEM;
+        } else	{
+
+          X509_STORE_CTX_set_ex_data(csc, PVD_STORE_EX_DATA_IDX, pvd);
+          /* X509_STORE_CTX_get0_param() only returns NULL if
+           * X509_STORE_CTX_init() has failed  */
+
+          if (verificationtime){
+
+            X509_VERIFY_PARAM_set_time(X509_STORE_CTX_get0_param(csc),
+                verificationtime);
+          }
+
+          index = X509_verify_cert(csc);
+        }
       }
     }
+
 #ifdef SIGPIPE
     signal(SIGPIPE, oldsignal);
 #endif
+
     destroy_initializers(pvd);
   }
+  
+  if (!index){
+    // Certificate validation failed, fetch underlying error
+    std::string vfy_error("Certificate verification failed ");
+
+    X509 *invalid_cert = X509_STORE_CTX_get_current_cert(csc);
+    if (invalid_cert){
+
+      vfy_error += "for certificate '";
+      char *cert_subject = X509_NAME_oneline(X509_get_subject_name(invalid_cert), NULL, 0);
+      vfy_error += cert_subject; 
+      vfy_error += "': ";
+      OPENSSL_free(cert_subject);
+    } 
+
+    vfy_error += X509_verify_cert_error_string(X509_STORE_CTX_get_error(csc));
+    vfy_error += ".";
+    seterror(VERR_VERIFY, vfy_error); 
+  }
+
   X509_STORE_free(ctx);
 
   if (csc)

src/server/vomsd.cc
@@ -1015,24 +1015,50 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
     }
   }
 
+  // Adjust for long/short format
+  if (!shortfqans && !fqans.empty()) {
+
+    LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "Translating FQANs to long format.");
+    std::vector<std::string> newfqans(fqans);
+    fqans.clear();
+    std::vector<std::string>::iterator i = newfqans.begin();
+    std::vector<std::string>::iterator end = newfqans.end();
+
+    while (i != end) {
+      std::string fqan = *i;
+      if (fqan.find("/Role=") != std::string::npos)
+        fqan += "/Capability=NULL";
+      else
+        fqan += "/Role=NULL/Capability=NULL";
+      LOGM(VARP, logh, LEV_DEBUG, T_PRE, "Translated FQAN: %s", fqan.c_str());
+      fqans.push_back(fqan);
+      ++i;
+    }
+  }
 
   if(!fqans.empty()) {
     
     LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "Checking if user comes with valid fqans.");
 
     vomsdata vd("", "");
-    vd.SetVerificationType((verify_type)(VERIFY_SIGN));
-    bool retrieve_success = vd.Retrieve(sock.actual_cert, sock.peer_stack, RECURSE_DEEP);
+    vd.SetVerificationType((verify_type)(VERIFY_SIGN | VERIFY_DATE));
     
-    LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "FQAN retrieve_success: %s", retrieve_success ? "true" : "false");
+    if (!vd.Retrieve(sock.actual_cert, sock.peer_stack, RECURSE_DEEP)){
+
+      std::string voms_error = vd.ErrorMessage();
+
+      LOGM(VARP, logh, LEV_DEBUG, T_PRE,  
+        "No valid VOMS attributes found in client cert chain. VOMS retrieve error: %s",
+        voms_error.c_str());
+    }
 
     std::vector<std::string> existing;
     std::vector<voms>::iterator end = (vd.data).end();
+
     for (std::vector<voms>::iterator index = (vd.data).begin(); index != end; ++index) 
     {
       if (index->voname == voname)
       {
-
         std::vector<std::string>::iterator fqan_it = index->fqan.begin();
 
         for ( ; fqan_it != index->fqan.end(); ++fqan_it)
@@ -1040,35 +1066,9 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
           LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "Found fqan in user credential: %s", fqan_it->c_str());
           existing.push_back(*fqan_it);
         }
-          
-        /*
-        existing.insert(existing.end(),
-            index->fqan.begin(),
-            index->fqan.end());
-        */
       }
     }
 
-    // Adjust for long/short format
-    if (!shortfqans && !fqans.empty()) {
-      
-      LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "Translating FQANs to long format.");
-      std::vector<std::string> newfqans(fqans);
-      fqans.clear();
-      std::vector<std::string>::iterator i = newfqans.begin();
-      std::vector<std::string>::iterator end = newfqans.end();
-
-      while (i != end) {
-        std::string fqan = *i;
-        if (fqan.find("/Role=") != std::string::npos)
-          fqan += "/Capability=NULL";
-        else
-          fqan += "/Role=NULL/Capability=NULL";
-        LOGM(VARP, logh, LEV_DEBUG, T_PRE, "Translated FQAN: %s", fqan.c_str());
-        fqans.push_back(fqan);
-        ++i;
-      }
-    }
 
     /* if attributes were found, only release an intersection beetween the requested and the owned */
     std::vector<std::string>::iterator fend = fqans.end();
@@ -1105,17 +1105,23 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
 
     if(subset) 
     {
-      LOG(logh, LEV_ERROR, T_PRE, "Error in executing request!");
-      vr.setError(WARN_ATTR_SUBSET, voname + " : your certificate already contains attributes, only a subset of them can be issued.");
+      LOG(logh, LEV_WARN, T_PRE, "Only a subset of the requested attributes will be issued.");
+      vr.setError(WARN_ATTR_SUBSET, voname + 
+      " : your certificate already contains attributes, only a subset of them can be issued.");
     }
   }
 
-  if (!fqans.empty()) {
-    // test logging retrieved attributes
+  if (fqans.empty()) {
+
+    vr.setError(ERR_NOT_MEMBER, std::string("You are not a member of the ") + voname + " VO!");
+    return false;
+
+  } else {
+
     std::vector<std::string>::const_iterator end = fqans.end();
 
     for (std::vector<std::string>::const_iterator i = fqans.begin(); i != end; ++i)
-      LOGM(VARP, logh, LEV_INFO, T_PRE, "Request Result: %s",  (*i).c_str());
+      LOGM(VARP, logh, LEV_INFO, T_PRE, "Issued FQAN: %s",  (*i).c_str());
 
     if (LogLevelMin(logh, LEV_DEBUG)) {
       if(result && !attribs.empty()) {
@@ -1127,12 +1133,18 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
         LOGM(VARP, logh, LEV_DEBUG, T_PRE,  "No generic attributes found for user.");
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     std::string codedac;
     std::string data;
 
-    if (comm[0] != "N") {
+    if (comm[0] == "N") {
+
+      std::vector<std::string>::const_iterator end = fqans.end();
+      for (std::vector<std::string>::const_iterator i = fqans.begin(); i != end; ++i)
+        data += (*i).c_str() + std::string("\n");
+
+    } else {
+
+      // This is the real AC encoding
       int res = 1;
       BIGNUM * serial = get_serial();
 
@@ -1183,13 +1195,6 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
         return false;
       }
     }
-    else {
-      /* comm[0] == "N" */
-
-      std::vector<std::string>::const_iterator end = fqans.end();
-      for (std::vector<std::string>::const_iterator i = fqans.begin(); i != end; ++i)
-        data += (*i).c_str() + std::string("\n");
-    }
 
     (void)SetCurLogType(logh, T_RESULT);
 
@@ -1197,10 +1202,7 @@ bool VOMSServer::makeAC(vomsresult& vr, EVP_PKEY *key, X509 *issuer,
     vr.setData(data);
     return true;
   }
-  else {
-    vr.setError(ERR_NOT_MEMBER, std::string("You are not a member of the ") + voname + " VO!");
-    return false;
-  }
+
 }
 
 void