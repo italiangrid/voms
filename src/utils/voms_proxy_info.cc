13cc636c962de5e228fa4343a078308da206518d
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2025-03-22 17:37:30 +0100

    Make sure to print the AC verification failure message
    Also make sure to return failure from the test_proxy function if the AC
    verification is requested, i.e. if -dont-verify-ac is not specified,
    and the verification fails. This causes voms-proxy-info to also return
    failure in such a case, as it was always supposed to do.
    
    Although returning failure is a bug fix, it is in practice also a
    backwards incompatibility and an incompatibility with the Java version
    of this utility, which doesn't verify the AC at all.

--- src/utils/voms_proxy_info.cc
+++ src/utils/voms_proxy_info.cc
@@ -405,31 +405,28 @@ test_proxy()
 
       vomsdata d("","");
       if (!dont_verify_ac) {
-          d.SetVerificationType((verify_type)(VERIFY_SIGN | VERIFY_KEY));
-          res = d.Retrieve(x, chain, RECURSE_CHAIN);
-      }
-      if (dont_verify_ac || !res || d.error == VERR_NOEXT) {
-        d.data.clear();
-        d.SetVerificationType((verify_type)(VERIFY_NONE));
+        d.SetVerificationType(static_cast<verify_type>(VERIFY_SIGN | VERIFY_KEY));
         res = d.Retrieve(x, chain, RECURSE_CHAIN);
-        if ( dont_verify_ac || d.error == VERR_NOEXT ) {
-            res = true;
+        // no VOMS extension is not an error
+        if (d.error == VERR_NOEXT) {
+          res = true;
+        }
+        if (!res) {
+          std::cerr << "\nWARNING: " << d.ErrorMessage() << "\n\n";
+        }
+      }
+      if (dont_verify_ac || !res) {
+        d.data.clear();
+        d.SetVerificationType(VERIFY_NONE);
+        d.Retrieve(x, chain, RECURSE_CHAIN);
+        if (dont_verify_ac || d.error == VERR_NOEXT) {
+          res = true;
         }
       }
 
-      if (!res) {
-        std::cerr << "WARNING: Unable to verify signature! Server certificate possibly not installed.\n" 
-                  << "Error: " << d.ErrorMessage() << std::endl;
-
-      }
-
-      bool print_res = print(x, chain, d);
-      if (print_res == false) {
-          res = false;
-      }
-    }
-    else {
-      std::cerr << std::endl << "Couldn't find a valid proxy." << std::endl << std::endl;
+      res = print(x, chain, d) && res;
+    } else {
+      std::cerr << "\nCouldn't find a valid proxy.\n\n";
       goto err;
     }
   }
