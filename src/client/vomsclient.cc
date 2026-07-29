italiangrid/voms

commit bd88b70843f54b474e271b902176d192e5b2c404

Refactor ASN.1 string handling
Introduce a utility function to_string that converts an ASN1_STRING into
an std::string. The function is defined in a new internal header file
asn1_utils.h, inside the new voms_internal namespace.

Note that many ASN1 string-like types are all typedefs of the same
underlying type, including ASN1_TIME, which however needs special
treatment.

src/client/vomsclient.cc
@@ -54,7 +54,6 @@ extern "C" {
 #include <algorithm>
 #include <string>
 #include <random>
-#include <iomanip>
 #include <cassert>
 #include <memory>
 
@@ -67,6 +66,7 @@ extern "C" {
 #include "vomsclient.h"
 #include "fqan.h"
 #include "contact.hpp"
+#include "asn1_utils.h"
 
 
 extern "C" 
@@ -792,28 +792,6 @@ int Client::Run()
   return 1;
 }
 
-namespace {
-
-// generate a string preferably in local time, with TZ indication
-std::string to_string(const ASN1_TIME *time)
-{
-  assert(time != nullptr);
-
-  tm tm_utc;
-  ASN1_TIME_to_tm(time, &tm_utc);
-  std::ostringstream os;
-#ifdef HAVE_TIMEGM
-  time_t t_utc = timegm(&tm_utc);
-  tm *tm_ptr = localtime(&t_utc);
-  os << std::put_time(tm_ptr, "%c %Z");
-#else
-  os << std::put_time(&tm_utc, "%c GMT");
-#endif
-  return os.str();
-}
-
-}
-
 bool Client::CreateProxy(std::string data, AC ** aclist, int version) 
 {
   using ArgsPtr = std::unique_ptr<VOMSProxyArguments, void (*)(VOMSProxyArguments *)>;
@@ -897,7 +875,7 @@ bool Client::CreateProxy(std::string data, AC ** aclist, int version)
         {
           Print(INFO) << "\nCreated proxy in " << proxyfile
                       << ".\n\nYour proxy is valid until "
-                      << to_string(X509_get0_notAfter(proxy->cert)) << '\n';
+                      << voms_internal::to_string(X509_get0_notAfter(proxy->cert)) << '\n';
         }
       }
     } else {
@@ -1110,7 +1088,7 @@ static bool check_validity_dates(X509 const* cert, int& time_left, std::string& 
   }
 
   if (start_cmp > 0) {
-    error = "Certificate is not yet valid; validity starts on " + to_string(not_before);
+    error = "Certificate is not yet valid; validity starts on " + voms_internal::to_string(not_before);
     return false;
   }
 
@@ -1119,7 +1097,7 @@ static bool check_validity_dates(X509 const* cert, int& time_left, std::string& 
   time_left = days * 24 * 60 * 60 + secs;
 
   if (end_cmp < 0) {
-    error = "Certificate has expired on " + to_string(not_after);
+    error = "Certificate has expired on " + voms_internal::to_string(not_after);
     return false;
   }
 

src/include/asn1_utils.h
@@ -1,0 +1,47 @@
+#ifndef VOMS_ASN1_UTILS_H
+#define VOMS_ASN1_UTILS_H
+
+#include <openssl/asn1.h>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+namespace voms_internal {
+
+// generate a string preferably in local time, with TZ indication
+inline std::string asn1_time_to_string(ASN1_TIME const* time)
+{
+  tm tm_utc;
+  ASN1_TIME_to_tm(time, &tm_utc);
+  std::ostringstream os;
+#ifdef HAVE_TIMEGM
+  time_t t_utc = timegm(&tm_utc);
+  tm* tm_ptr = localtime(&t_utc);
+  os << std::put_time(tm_ptr, "%c %Z");
+#else
+  os << std::put_time(&tm_utc, "%c GMT");
+#endif
+  return os.str();
+}
+
+inline std::string to_string(ASN1_STRING const* value)
+{
+  if (value == nullptr) {
+    return {};
+  }
+
+  // special case a time. note that ASN1_TIME is a typedef for ASN1_STRING
+  auto type = ASN1_STRING_type(value);
+  if (type == V_ASN1_GENERALIZEDTIME || type == V_ASN1_UTCTIME) {
+    return asn1_time_to_string(value);
+  }
+
+  return std::string{
+    reinterpret_cast<char const*>(ASN1_STRING_get0_data(value)),
+    static_cast<std::string::size_type>(ASN1_STRING_length(value))
+  };
+}
+
+} // namespace voms_internal
+
+#endif

src/ac/validate.cc
@@ -64,6 +64,7 @@ extern "C" {
 
 #include "../api/ccapi/voms_api.h"
 #include "../api/ccapi/realdata.h"
+#include "asn1_utils.h"
 
 #include <string>
 
@@ -206,8 +207,7 @@ int validate(X509 *cert, X509 *issuer, AC *ac, voms &v, verify_type valids, time
 
   v.version    = 1;
   v.siglen     = ASN1_STRING_length(ac->signature);
-  v.signature  = std::string((const char*) ASN1_STRING_get0_data(ac->signature),
-                             ASN1_STRING_length(ac->signature));
+  v.signature  = voms_internal::to_string(ac->signature);
   bn           = ASN1_INTEGER_to_BN(ac->acinfo->serial, NULL);
   char *bnstring = BN_bn2hex(bn);
   v.serial     = std::string(bnstring);
@@ -302,10 +302,8 @@ int validate(X509 *cert, X509 *issuer, AC *ac, voms &v, verify_type valids, time
   b = ac->acinfo->validity->notBefore;
   a = ac->acinfo->validity->notAfter;
 
-  v.date1 = std::string((const char*) ASN1_STRING_get0_data(b),
-                        ASN1_STRING_length(b));
-  v.date2 = std::string((const char*) ASN1_STRING_get0_data(a),
-                        ASN1_STRING_length(a));
+  v.date1 = voms_internal::to_string(b);
+  v.date2 = voms_internal::to_string(a);
 
   if (valids & VERIFY_DATE) {
     time_t ctime, dtime;
@@ -382,8 +380,7 @@ static int checkAttributes(STACK_OF(AC_ATTR) *atts, voms &v)
   /* put policyAuthority in voms struct */
   data = sk_GENERAL_NAME_value(capattr->names, 0);
   if (data->type == GEN_URI) {
-    v.voname = std::string((const char*) ASN1_STRING_get0_data(data->d.ia5),
-                           ASN1_STRING_length(data->d.ia5));
+    v.voname = voms_internal::to_string(data->d.ia5);
     std::string::size_type point = v.voname.find("://");
 
     if (point != std::string::npos) {
@@ -405,8 +402,7 @@ static int checkAttributes(STACK_OF(AC_ATTR) *atts, voms &v)
     if (!(ASN1_STRING_type(capname) == V_ASN1_OCTET_STRING))
       return AC_ERR_ATTRIB_FQAN;
 
-    std::string str = std::string((const char*) ASN1_STRING_get0_data(capname),
-                                  ASN1_STRING_length(capname));
+    std::string str = voms_internal::to_string(capname);
     std::string::size_type top_group_size = top_group.size();
     std::string::size_type str_size = str.size();
 
@@ -636,19 +632,15 @@ static int interpret_attributes(AC_FULL_ATTRIBUTES *full_attr, realdata *rd)
       AC_ATTRIBUTE *at = sk_AC_ATTRIBUTE_value(atts, j);
 
       struct attribute a;
-      a.name      = std::string((const char*) ASN1_STRING_get0_data(at->name),
-                                ASN1_STRING_length(at->name));
-      a.value     = std::string((const char*) ASN1_STRING_get0_data(at->value),
-                                ASN1_STRING_length(at->value));
-      a.qualifier = std::string((const char*) ASN1_STRING_get0_data(at->qualifier),
-                                ASN1_STRING_length(at->qualifier));
+      a.name      = voms_internal::to_string(at->name);
+      a.value     = voms_internal::to_string(at->value);
+      a.qualifier = voms_internal::to_string(at->qualifier);
 
       al.attributes.push_back(a);
     }
 
     gn = sk_GENERAL_NAME_value(holder->grantor, 0);
-    al.grantor = std::string((const char*) ASN1_STRING_get0_data(gn->d.ia5),
-                             ASN1_STRING_length(gn->d.ia5));
+    al.grantor = voms_internal::to_string(gn->d.ia5);
 
     rd->attributes->push_back(al);
   }

src/api/ccapi/api_util.cc
@@ -25,6 +25,7 @@
 
 #include "config.h"
 #include "api_util.h"
+#include "asn1_utils.h"
 
 extern "C" {
 #include "replace.h"
@@ -165,8 +166,7 @@ static bool findexts(X509 *cert , AC_SEQ **listnew, std::string &extra_data, std
   if (ext) {
     const ASN1_OCTET_STRING* value = X509_EXTENSION_get_data(ext);
     assert(value && "X509_EXTENSION_get_data failed");
-    extra_data = std::string((const char*) ASN1_STRING_get0_data(value),
-                             ASN1_STRING_length(value));
+    extra_data = voms_internal::to_string(value);
     found = true;
   }
 
@@ -174,8 +174,7 @@ static bool findexts(X509 *cert , AC_SEQ **listnew, std::string &extra_data, std
   if (ext) {
     const ASN1_OCTET_STRING* value = X509_EXTENSION_get_data(ext);
     assert(value && "X509_EXTENSION_get_data failed");
-    workvo = std::string((const char*) ASN1_STRING_get0_data(value),
-                         ASN1_STRING_length(value));
+    workvo = voms_internal::to_string(value);
   }
 
   return found;
@@ -431,8 +430,7 @@ vomsdata::check(void *data)
     return NULL;
   }
   
-  std::string voname((const char*) ASN1_STRING_get0_data(name->d.ia5), 0,
-                     ASN1_STRING_length(name->d.ia5));
+  std::string voname = voms_internal::to_string(name->d.ia5);
   std::string::size_type cpos = voname.find("://");
   std::string hostname;
 

src/api/ccapi/voms_api.cc
@@ -77,6 +77,7 @@ extern int InitProxyCertInfoExtension(int);
 #include <sstream>
 
 #include <voms_api.h>
+#include "asn1_utils.h"
 #include "data.h"
 #include "vomsxml.h"
 
@@ -1021,8 +1022,7 @@ std::vector<std::string> voms::GetTargets()
         AC_TARGET *name = NULL;
         name = sk_AC_TARGET_value(target->targets, i);
         if (name->name->type == GEN_URI)
-          targets.push_back(std::string((const char*) ASN1_STRING_get0_data(name->name->d.ia5),
-                                        ASN1_STRING_length(name->name->d.ia5)));
+          targets.push_back(voms_internal::to_string(name->name->d.ia5));
       }
     }
     AC_TARGETS_free(target);

src/include/Makefile.am
@@ -12,6 +12,6 @@ options.h pass.h Server.h fqan.h doio.h \
 vomsxml.h errors.h log.h sslutils.h normalize.h \
 listfunc.h credentials.h newformat.h proxycertinfo.h proxypolicy.h \
 acstack.h validate.h ccac.h init.h ccwrite.h getopts.h replace.h dbwrap.h \
-stamp-h.in stamp-h1.in ssl_compat.h
+stamp-h.in stamp-h1.in ssl_compat.h asn1_utils.h
 
 MAINTAINERCLEANFILES = Makefile.in

commit 9a31c8b39c962ed19240eb6b29b56c7ca4e8b955

Call explicitly the friendly stringification of an ASN1_TIME
An ASN1_TIME, which is a typedef for an ASN1_STRING, can be stringified
in multiple ways; don't assume which one is wanted. In particular,
use explicitly the friendly format when printing the validity dates of
the proxy just obtained from voms-proxy-init, but rely on the compact
format when used internally, for example in voms-proxy-info when
obtaining the date to be then passed to ASN1_TIME_set_string_X509.
This fixes a segmentation fault introduced in commit bd88b70.

src/client/vomsclient.cc
@@ -857,7 +857,7 @@ bool Client::CreateProxy(std::string data, AC ** aclist, int version)
         {
           Print(INFO) << "\nCreated proxy in " << proxyfile
                       << ".\n\nYour proxy is valid until "
-                      << voms_internal::to_string(X509_get0_notAfter(proxy->cert)) << '\n';
+                      << voms_internal::asn1_time_to_string(X509_get0_notAfter(proxy->cert)) << '\n';
         }
       }
     } else {
@@ -1070,7 +1070,7 @@ static bool check_validity_dates(X509 const* cert, int& time_left, std::string& 
   }
 
   if (start_cmp > 0) {
-    error = "Certificate is not yet valid; validity starts on " + voms_internal::to_string(not_before);
+    error = "Certificate is not yet valid; validity starts on " + voms_internal::asn1_time_to_string(not_before);
     return false;
   }
 
@@ -1079,7 +1079,7 @@ static bool check_validity_dates(X509 const* cert, int& time_left, std::string& 
   time_left = days * 24 * 60 * 60 + secs;
 
   if (end_cmp < 0) {
-    error = "Certificate has expired on " + voms_internal::to_string(not_after);
+    error = "Certificate has expired on " + voms_internal::asn1_time_to_string(not_after);
     return false;
   }
 

src/include/asn1_utils.h
@@ -30,12 +30,6 @@ inline std::string to_string(ASN1_STRING const* value)
     return {};
   }
 
-  // special case a time. note that ASN1_TIME is a typedef for ASN1_STRING
-  auto type = ASN1_STRING_type(value);
-  if (type == V_ASN1_GENERALIZEDTIME || type == V_ASN1_UTCTIME) {
-    return asn1_time_to_string(value);
-  }
-
   return std::string{
     reinterpret_cast<char const*>(ASN1_STRING_get0_data(value)),
     static_cast<std::string::size_type>(ASN1_STRING_length(value))